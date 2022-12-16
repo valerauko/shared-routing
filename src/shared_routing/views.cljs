ca62837e1f9977a9fbeb594e9ba56ae5a5f641ed
Vale <valerauko@gmail.com>
2022-12-16 22:04:49 +0900

    Make view routable

--- src/shared_routing/views.cljs
+++ src/shared_routing/views.cljs
@@ -1,5 +1,18 @@
-(ns shared-routing.views)
+(ns shared-routing.views
+  (:require [re-frame.core :as rf]
+            [shared-routing.subs :as subs]
+            [shared-routing.views.home :refer [home]]))
 
 (defn main
   []
-  [:div "Hello, world"])
+  (let [{{:keys [view]} :data :as route}
+        @(rf/subscribe [::subs/current-route])]
+    [:div
+     [:h1
+      "Shared routing example"]
+     [:p
+      (str "Route: " (get-in route [:data :name] "[missing]"))]
+     (if (fn? view)
+       [view]
+       [:p
+        "Not found!"])]))
